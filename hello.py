896924d9743a6471c7c79c28ea77466f18f738b9
ThePeem <pablorenzo98@gmail.com>
2025-01-10 12:51:43 +0100

    commit inicial

---
+++ hello.py
@@ -0,0 +1,17 @@
+# Aqui escribo lo que me de la gana 
+# Hola mundo
+print("Hola josema")
+
+"""
+Esto son solo comentarios
+aqui esto
+aqui nada
+"""
+
+# Consultar el tipo de dato
+
+print(type(5))# int
+print(type(5.5)) # float
+
+# Funcion para sumar numeros
+
