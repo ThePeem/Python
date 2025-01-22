756ffbff3575ed8a79b19b7d3009be1d10536e60
ThePeem <pablorenzo98@gmail.com>
2025-01-22 12:08:40 +0100

    Comienzo del curso

--- hello.py
+++ hello.py
@@ -17,3 +17,43 @@ print(type(5.5)) # float
 
 # hola
 # Cambio de la nueva rama
+
+# Adivinar numeros
+'''
+guess = 0
+
+while guess != 6:
+  guess = int(input("Guess the number:  "))
+
+print("You got it!")
+'''
+
+# Bucles con for
+"""
+i = 99
+
+for i in range(99, 0, -1):  # Comienza en 99, termina en 1, decrementando en pasos de -1
+    print(f"{i} bottles of beer on the wall.")  # Interpolación de la variable i
+    print(f"{i} bottles of beer.")
+    print("Take one down, pass it around,")
+    print(f"{i-1} bottles of beer on the wall.\n")  # Cálculo de i-1 para la siguiente iteración
+
+"""
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+num = int(12)
+coma = float(4)
+text = str("is the best place to learn and practice coding!")
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + num)
+# Print the sum of the double variables on a new line.
+print(d + coma)
+# Concatenate and print the String variables on a new line
+print(s," ",text)
+# The 's' variable above should be printed first.
+
