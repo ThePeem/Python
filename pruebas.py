701663a5bb3d2529f272c51e77f8f879fdd55fd9
ThePeem <pablorenzo98@gmail.com>
2025-01-29 15:33:57 +0100

    Ejercicios del curso

--- pruebas.py
+++ pruebas.py
@@ -1,12 +1,17 @@
-n = int(input())
+"""
+LA SUCESIÓN DE FIBONACCI
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en 0.
+- La serie Fibonacci se compone por una sucesión de números en
+  la que el siguiente siempre es la suma de los dos anteriores.
+  0, 1, 1, 2, 3, 5, 8, 13...
+"""
+num1=0
+num2=0
 
-if n & 2 != 0:
-    print("Weird")
-elif n > 2 and n < 5:
-    print("Not Weird")
-elif n > 6 and n < 20:
-    print("Weird")
-elif n > 20:
-    print("Not Weird")
-else :
-    print("Not Weird")
+for i in range(0,51):
+    num1 = i 
+    num2 = i -1
+    print(num1 + num2)
+
+
